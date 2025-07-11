d015774d26c70541771e318f738d4a878373f525
Angel Sixto <angel.sixtovl@gmail.com>
2025-07-10 19:42:50 -0600

    Usar process.env.PORT para deploy

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 8080;
 
 // Middlewares
 app.use(morgan("dev"));
@@ -52,4 +51,4 @@ app.use("/",(req,res)=>{
 // ==============================
 app.listen(port, () => {
     console.log(`Sitio web corriendo en http://localhost:${port}`);
-});
+})
